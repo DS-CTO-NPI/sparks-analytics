ffcba706b00a75d65cd132b58568c2f02e3cbf1a
Shrikant Kale <shrikant.kale@lntecc.com>
2024-06-30 08:45:17 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'des-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sparks-analytics';
+}
